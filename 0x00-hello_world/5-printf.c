4ebc1e1fa13facd59cdb84bcb1a04fb1afb88970
Thozama <clara.thozama@gmail.com>
2022-06-03 01:18:34 -0700

    Printing a string

--- 0x00-hello_world/5-printf.c
+++ 0x00-hello_world/5-printf.c
@@ -1,7 +1,9 @@
 #include <stdio.h>
+
 /**
  * main - Prints a string
- * Description: Prints the exact words of given text
+ *
+ * Description: It prints the exact words of given text
  *
  * Return: 0
  *
@@ -9,5 +11,5 @@
 int main(void)
 {
 printf("\"with proper grammar, but the outcome is a piece of art,\n");
-Return: 0;
+Return: (0);
 }
