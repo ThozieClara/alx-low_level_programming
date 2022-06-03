f304228c86829ec3688457cda260c971ac53c7f3
Thozama <clara.thozama@gmail.com>
2022-06-02 18:31:41 -0700

    creating a c_file

--- 0x00-hello_world/4-puts.c
+++ 0x00-hello_world/4-puts.c
@@ -1,12 +1,12 @@
 #include <stdio.h>
 /**
- * main - print the string in the put function
+ * main - print the string in the puts function
  *
  * Description: prints "Programming is like building a multilingual puzzle"
  * Return: 0
 */
 int main(void)
 {
-put("\"Programming is like building a multilingual puzzle");
+put("\"Programming is like building a multilingual puzzle\n");
 return (0);
 }
