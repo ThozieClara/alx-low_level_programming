aef377bebd5025a541203e982d4e626417bc363c
Thozama <clara.thozama@gmail.com>
2022-06-03 00:25:47 -0700

    printf_print_exact_text

---
+++ 0x00-hello_world/5-printf.c
@@ -0,0 +1,11 @@
+#include <stdio.h>
+/**
+ * main - Prints the exact words of given text
+ * Description: with proper grammar, but the outcome is a piece of art
+ * Return: 0
+*/
+int main(void)
+{
+printf("with proper grammar, but the outcome is a piece of art,\n");
+Return(0);
+}
