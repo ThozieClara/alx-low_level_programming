511f7c9646ca056af0a5fbcdb4848aa7bf52271b
Thozama <clara.thozama@gmail.com>
2022-06-03 14:06:25 -0700

    test negative or possitive

---
+++ 0x01-variables_if_else_while/1-last_digit.c
@@ -0,0 +1,27 @@
+#include <stdlib.h>
+#include <time.h>
+#include <stdio.h>
+/**
+ * main - to start code
+ *
+ * description: check whether positive or negative
+ *
+ * Return: 0;
+ *
+*/
+
+int main(void)
+{
+        int n;
+
+        srand(time(0));
+        n = rand() - RAND_MAX / 2;
+        if (n > 0)
+        {
+                printf("%dis positive\n", n);
+        } else if (n == 0)
+        {
+                printf("%dis zero\n", n);
+        } else if (n < 0)
+        printf("%dis negative\n", n);
+        return (0);
