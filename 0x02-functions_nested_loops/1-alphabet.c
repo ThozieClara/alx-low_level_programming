c277b527231d77a680ddb9b1b815a1052e1a8d10
Thozama <clara.thozama@gmail.com>
2022-06-08 18:55:48 -0700

    print alphabet in lowercase

--- 0x02-functions_nested_loops/1-alphabet.c
+++ 0x02-functions_nested_loops/1-alphabet.c
@@ -6,16 +6,14 @@
  * Description: prints alphabet in lowercase
  *
  * Return: 0 is successful
- */
+*/
+
 void print_alphabet(void)
 {
 int bet;
 
-for (bet = 'a'; bet <= 'z'; bet++);
-{
+for (bet = 'a'; bet <= 'z'; bet++)
 _putchar(bet);
-
 _putchar('\n');
-}
 return (0);
 }
