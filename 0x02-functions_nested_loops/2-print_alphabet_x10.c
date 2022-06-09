e5301d4bb56a6aec86033a91623a9703f8e5dc73
Thozama <clara.thozama@gmail.com>
2022-06-08 19:40:04 -0700

    a function that prints 10 times the alphabet

--- 0x02-functions_nested_loops/2-print_alphabet_x10.c
+++ 0x02-functions_nested_loops/2-print_alphabet_x10.c
@@ -4,8 +4,10 @@
  * print_alphabet_x10 - Prints 10 times the alphabet, in lowercase.
  *
  * Description: prints alphabet 10 times
+ *
+ * Return: always 0.
 */
-void print_alphabet_x10((void)
+void print_alphabet_x10(void)
 {
 int count = 0;
 char bet;
@@ -14,7 +16,6 @@ char bet;
 	{
 		for (bet = 'a'; bet <= 'z'; bet++)
 			_putchar(bet);
-		_putchar('\n');
-		return(0);
 	}
+		_putchar('\n');
 }
